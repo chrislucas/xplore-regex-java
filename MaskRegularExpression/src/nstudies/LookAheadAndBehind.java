8da4bc3ed81a8afb2164d5f54826431118c31475
Christoffer Lucas <christoffer.luccas@gmail.com>
2018-05-29 05:49:47 -0300

    testando lookaround

--- MaskRegularExpression/src/nstudies/LookAheadAndBehind.java
+++ MaskRegularExpression/src/nstudies/LookAheadAndBehind.java
@@ -5,9 +5,22 @@ public class LookAheadAndBehind {
 
     /**
      * http://www.regular-expressions.info/lookaround.html
+     *
+     * Negative lookahead (?!regex)
+     * Positive lookahead (?=regex)
+     *
      * */
 
+    private static void test() {
+        String neg = "q(?!u)\\.*";
+        String pos = "q(?=u)\\.*";
+        System.out.println("qu".matches(neg));
+        System.out.println("qu".matches(pos));
+    }
+
     public static void main(String[] args) {
+
+        test();
 
     }
 
